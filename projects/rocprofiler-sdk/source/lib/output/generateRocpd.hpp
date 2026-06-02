eea7e69ce48636956b9f6e4c00832a405b193b0c
EvanPhoukong <Evan.Phoukong@amd.com>
2026-06-02 20:35:30 +0000

    rocprofv3: Support rocSHMEM EXT buffer records (args + retval)

--- projects/rocprofiler-sdk/source/lib/output/generateRocpd.hpp
+++ projects/rocprofiler-sdk/source/lib/output/generateRocpd.hpp
@@ -51,7 +51,7 @@ write_rocpd(
     const generator<tool_buffer_tracing_kfd_record_t>&                      kfd_gen,
     const generator<rocprofiler_buffer_tracing_rccl_api_record_t>&          rccl_api_gen,
     const generator<rocprofiler_buffer_tracing_rocdecode_api_ext_record_t>& rocdecode_api_gen,
-    const generator<rocprofiler_buffer_tracing_rocshmem_api_record_t>&      rocshmem_api_gen,
+    const generator<rocprofiler_buffer_tracing_rocshmem_api_ext_record_t>&  rocshmem_api_gen,
     const generator<tool_counter_record_t>&                                 counter_collection_gen,
     const generator<tool_spm_counter_record_t>&                             spm_collection_gen,
     const generator<rocprofiler_buffer_tracing_ompt_record_t>&              ompt_gen);
