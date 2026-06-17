44177039c52d30ad4861e87c7c43ef68c4d8dd6b
Lancelot SIX <lancelot.six@amd.com>
2026-06-17 15:22:50 +0100

    rocr: fix trap_handler_gfx12 invalid branch
    The current gfx12 trap handler has an error login when looking for
    WAVE_START / WAVE_END traps.  If the trap handler detect that the
    wave_start exception was not raised, it skips past after the part to
    check wave_end, effectively skipping wave end.
    
    Neither of those exceptions are actively used by any product today, so
    fixing it has no real impact.  However, as the bug was noticed, we might
    as well fix it.

--- projects/rocr-runtime/runtime/hsa-runtime/core/runtime/trap_handler/trap_handler_gfx12.s
+++ projects/rocr-runtime/runtime/hsa-runtime/core/runtime/trap_handler/trap_handler_gfx12.s
@@ -312,7 +312,7 @@
 
 .not_illegal_instruction:
   s_bitcmp1_b32     ttmp2, SQ_WAVE_EXCP_FLAG_PRIV_WAVE_START_SHIFT
-  s_cbranch_scc0    .not_wave_end
+  s_cbranch_scc0    .not_wave_start
   s_or_b32          ttmp3, ttmp3, EC_QUEUE_WAVE_TRAP_M0
 
 .not_wave_start:
