55fe0d4ad5d9deabfa69ef22471a3097a25bde9a
kesag <keshav.aggarwal@amd.com>
2026-06-30 21:08:03 +0000

    skip test for ro

--- projects/rocshmem/scripts/functional_tests/driver.sh
+++ projects/rocshmem/scripts/functional_tests/driver.sh
@@ -719,9 +719,12 @@ TestColl() {
   ExecTest  "teambroadcast"    2       1            64        32768
 
   ExecTest  "fcollect"         2       1            64        32768
-  ExecTest  "fcollect_wave"    2       1            64        32768
 
   ExecTest  "teamreduction"    2       1            64        32768
+
+  if [[ $TEST != ro* ]]; then #AIROCSHMEM-432: wave tests not supported on ROA
+    ExecTest  "fcollect_wave"    2       1            64        32768
+  else echo "Skip:   *_wave (AIROCSHMEM-408: wave tests not supported on RO)"; fi
 }
 
 TestOnStream() {
